feb40678d6e3d3852250f4f30921849cf39ad599
AHIRAJ K <23bl15751@rit.ac.in>
2025-04-26 20:26:35 +0530

    area of circle code

---
+++ 01-Arithmetic/area_of_circle.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    const float PI = 3.14159;
+    float radius, area;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    if (radius < 0) {
+        printf("Error: Radius cannot be negative.\n");
+        return 1;
+    }
+
+    area = PI * radius * radius;
+
+    printf("Area of the circle is: %.2f\n", area);
+
+    return 0;
+}
