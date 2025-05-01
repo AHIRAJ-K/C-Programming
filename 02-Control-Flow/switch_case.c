096b94978d8b90f80c86a4f7a51e4cfc6f5dd9d2
AHIRAJ K <23bl15751@rit.ac.in>
2025-05-01 21:39:30 +0530

    changed code

--- 02-Control-Flow/switch_case.c
+++ 02-Control-Flow/switch_case.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    int choice;
-
+    int choice, a, b;
     printf("Menu:\n");
-    printf("1. Greet\n");
-    printf("2. Say Bye\n");
-    printf("3. Exit\n");
-    printf("Enter your choice (1-3): ");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Enter your choice (1-4): ");
     scanf("%d", &choice);
 
-    switch (choice) {
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    switch(choice) {
         case 1:
-            printf("Hello! Have a great day.\n");
+            printf("Result: %d\n", a + b);
             break;
         case 2:
-            printf("Goodbye! See you again.\n");
+            printf("Result: %d\n", a - b);
             break;
         case 3:
-            printf("Exiting program.\n");
+            printf("Result: %d\n", a * b);
+            break;
+        case 4:
+            if (b != 0)
+                printf("Result: %.2f\n", (float)a / b);
+            else
+                printf("Error: Division by zero not allowed.\n");
             break;
         default:
             printf("Invalid choice.\n");
