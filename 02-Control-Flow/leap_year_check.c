ccba59935580b88a447e1ab7d1b59425f7909778
AHIRAJ K <23bl15751@rit.ac.in>
2025-05-03 00:24:29 +0530

    code

---
+++ 02-Control-Flow/leap_year_check.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int year;
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Leap year if divisible by 4 and (not divisible by 100 or divisible by 400)
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
