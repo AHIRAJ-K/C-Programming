d51177838c21370cf506fe17a40867e276fcb08e
AHIRAJ K <23bl15751@rit.ac.in>
2025-05-05 21:20:22 +0530

    code

---
+++ 02-Control-Flow/print_reverse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num <= 0) {
+        printf("Please enter a positive number.\n");
+    } else {
+        printf("Digits in reverse order: ");
+        while (num > 0) {
+            printf("%d ", num % 10);
+            num /= 10;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
